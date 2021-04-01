8f64f4701510cff93d8dfbaeb025bd015670aea0
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-01 19:44:05 +0200

    ADD tarea-tipado file

---
+++ src/ejercicios/03-functiones.ts
@@ -0,0 +1,33 @@
+function sumar(a: number, b: number): number {
+  return a + b;
+}
+
+const sumarFlecha = (a: number, b: number): number => a + b;
+
+function multiplicar(
+  numero: number,
+  otroNumero?: number,
+  base: number = 2
+): number {
+  return numero * base;
+}
+
+interface PersonajeLOR {
+  nombre: string;
+  pv: number;
+  mostrarHP: () => void;
+}
+
+function curar(personaje: PersonajeLOR, curarX: number): void {
+  personaje.pv += curarX;
+}
+const nuevoPersonaje: PersonajeLOR = {
+  nombre: 'Strider',
+  pv: 50,
+  mostrarHP() {
+    console.log('Puntos de vida: ', this.pv);
+  },
+};
+
+curar(nuevoPersonaje, 100);
+nuevoPersonaje.mostrarHP();
