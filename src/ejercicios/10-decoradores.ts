44ab98077206415c12c111be33a3e77bc3f53ca0
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-04 15:20:30 +0200

    ADD genericos, decoradores and optional chaining

---
+++ src/ejercicios/10-decoradores.ts
@@ -0,0 +1,19 @@
+function reportableClassDecorator<T extends { new (...args: any[]): {} }>(
+  constructor: T
+) {
+  return class extends constructor {
+    newProperty = 'new property';
+    hello = 'override';
+  };
+}
+
+@reportableClassDecorator
+class MiSuperClase {
+  public miPropiedad: string = 'ABC123';
+  imprimir() {
+    console.log('Hola Mundo');
+  }
+}
+
+console.log(MiSuperClase);
+const miClase = new MiSuperClase();
